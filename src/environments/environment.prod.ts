e74c492d1faec530f576208435fab4d5a4e7a6b9
Levan Fredy MBOVING TEGUETIO <34085291+mbovingfred@users.noreply.github.com>
2021-03-12 08:45:08 +0100

    fixed environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  authApiUrl: 'https://accounts.nvne.fr/auth',
+  backendApiUrl: 'https://www.nvne.fr/api/espaceentreprise',
+  adminUrl: 'http://admin.nvne.fr',
+  homeUrl: 'https://www.nvne.fr/#',
+  enableDebug: false
+};
